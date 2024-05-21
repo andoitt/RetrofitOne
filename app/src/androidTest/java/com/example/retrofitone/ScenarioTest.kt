3bfe982cb57bc1ccc377958ea81f08c0d3eb7e74
Andrew Sutyrin <andoitt@gmail.com>
2024-05-21 13:55:35 +0300

    ScenarioTest

---
+++ app/src/androidTest/java/com/example/retrofitone/ScenarioTest.kt
@@ -0,0 +1,89 @@
+package com.example.retrofitone
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.retrofitone.load.LoadPage
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Rule
+
+/**
+ *TestCase
+ * 1.Первый запуск -> ProgressState
+ *
+ * 2. Ошибка ErrorState
+ *
+ * 3. Кликаем на RetryButton ->
+ *      ProgressState
+ * 4. Время закрузки ->
+ *      UserInfoState
+ * 5. Клик NextUserButton ->
+ *      ProgressState
+ */
+@RunWith(AndroidJUnit4::class)
+class ScenarioTest {
+
+    @get:Rule
+    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+
+  //  private lateinit var userPage: UserPage
+
+
+    @Test
+    fun testCase1() {
+        val loadPage = LoadPage()
+        loadPage.checkProgressState()
+        scenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        loadPage.waitUntilError()
+        loadPage.checkErrorState(message = "Internet connection problems")
+        scenarioRule.scenario.recreate()
+        loadPage.checkErrorState(message = "Internet connection problems")
+
+        loadPage.clickRetry()
+        loadPage.checkProgressState()
+        scenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        loadPage.waitUntilDisappear()
+        var userPage = UserPage(
+            gender = "male",
+            city = "Bela Palanka"
+
+        )
+        userPage.checkUserInfoState()
+        scenarioRule.scenario.recreate()
+        userPage.checkUserInfoState()
+
+        userPage.clickNextUser()
+        loadPage.checkProgressState()
+        loadPage.checkProgressState()
+        scenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        loadPage.waitUntilError()
+        loadPage.checkErrorState(message = "Internet connection problems")
+        scenarioRule.scenario.recreate()
+        loadPage.checkErrorState(message = "Internet connection problems")
+
+        loadPage.clickRetry()
+        loadPage.checkProgressState()
+        scenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        loadPage.waitUntilDisappear()
+        userPage = UserPage(
+            gender = "male",
+            city = "Bela Palanka"
+
+        )
+        userPage.checkUserInfoState()
+        scenarioRule.scenario.recreate()
+        userPage.checkUserInfoState()
+
+    }
+}
